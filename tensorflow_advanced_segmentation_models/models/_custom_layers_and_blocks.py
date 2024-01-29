2a124663bd35e361e6fa498e03781c78bb67646a
David Kazaryan <kazaryan.david@gmail.com>
2024-01-29 23:09:44 +0400

    Fix dtype casting in MixtureOfSoftMaxACF

--- tensorflow_advanced_segmentation_models/models/_custom_layers_and_blocks.py
+++ tensorflow_advanced_segmentation_models/models/_custom_layers_and_blocks.py
@@ -725,7 +725,7 @@ class GlobalPooling(tf.keras.layers.Layer):
 class MixtureOfSoftMaxACF(tf.keras.layers.Layer):
     def __init__(self, d_k, att_dropout=0.1, **kwargs):
         super(MixtureOfSoftMaxACF, self).__init__(**kwargs)
-        self.temperature = tf.cast(tf.math.pow(tf.cast(d_k, self.dtype), 0.5), self.dtype)
+        self.temperature = tf.cast(tf.math.pow(tf.cast(d_k, self.dtype), 0.5), self.compute_dtype)
         self.att_dropout = att_dropout
         
         self.dropout = tf.keras.layers.Dropout(att_dropout)
